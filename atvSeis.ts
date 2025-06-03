8eba274986e6fe54c318c71c72276d7ac19cdc5d
ismael henrique <ismaelhenrique756@gmail.com>
2025-06-03 09:28:11 -0300

    finalisação da atividade

--- atvSeis.ts
+++ atvSeis.ts
@@ -1,6 +1,7 @@
 // 6- Faça um programa que verifique e mostre os números entre
 // 1.000 e 2.000 (inclusive) que, quando divididos por 11
 // produzam resto igual a 2. Utilizar o laço de repetição FOR.
+// nome: ismael henrique cardoso dos santos//
 
 const teclado = require(`prompt-sync`)();
 
--- atvSete.ts
+++ atvSete.ts
@@ -1,6 +1,7 @@
 // 7- Faça um programa para calcular n! (Fatorial de
 //     n), sendo que o valor inteiro de n é fornecido pelo
 //     usuário. utilize laço de repetição for.
+// nome: ismael henrique cardoso dos santos//
 
 const teclado = require(`prompt-sync`)();
 
